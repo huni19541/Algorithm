a8505a473959f99f7fa471fc6cf4b98af10d57d5
huni19541 <133645725+huni19541@users.noreply.github.com>
2023-07-18 17:02:25 +0900

    Update 기능개발.java

--- 프로그래머스/lv2/42586. 기능개발/기능개발.java
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.java
@@ -4,13 +4,13 @@ class Solution {
     public static int[] solution(int[] progresses, int[] speeds) {
 		int[] answer = {};
         
-        // 값이 저장될 가변배열
+        	// 값이 저장될 가변배열
 		ArrayList<Integer> al = new ArrayList<>();
         
 		int idx = 0;
 		while(true)
 		{
-            // 매일 진도율 더하기
+            		// 매일 진도율 더하기
 			for(int i=0; i<speeds.length; i++)
 			{
 				progresses[i] += speeds[i];
@@ -19,7 +19,7 @@ class Solution {
 			
 			int cnt = 0;
 			
-            // 작업이 완성된 개수 구하기
+            		// 작업이 완성된 개수 구하기
 			while(progresses[idx] == 100)
 			{
 				cnt++;
@@ -28,19 +28,19 @@ class Solution {
 					break;
 			}
 			
-            // 작업이 완성된 개수 추가
+            		// 작업이 완성된 개수 추가
 			if(cnt != 0)
 				al.add(cnt);
 			
-            // 모든 작업 완성
+            		// 모든 작업 완성
 			if(idx == speeds.length)
 				break;
 		}
 		
-        // int[] 배열 선언
+       		// int[] 배열 선언
 		answer = new int[al.size()];
 		
-        // ArrayList값 옮기기
+        	// ArrayList값 옮기기
 		for(int i=0; i<al.size(); i++)
 			answer[i] = al.get(i);
 		
