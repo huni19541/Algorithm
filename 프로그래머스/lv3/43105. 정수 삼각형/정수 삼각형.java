85147539966cd696e468b1fe3aee6cc0e3a56eef
huni19541 <133645725+huni19541@users.noreply.github.com>
2023-07-18 17:04:51 +0900

    Update 정수 삼각형.java

--- 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,22 +1,25 @@
 public class Solution {
 	    public int solution(int[][] triangle) {
 	        int answer = 0;
-
+		
 	        int tl = triangle.length;
+
+		// 재사용 할 dp배열 선언
 	        int dp[][] = new int[tl][];
 	        dp[0] = triangle[0];
 	        
 	        for(int i=1; i<tl; i++)
 	        {
 	            dp[i] = new int[i+1];
-	            
+
+		    // 왼쪽과 오른쪽 중 최대값 더하기
 	            for(int j=0; j<i+1; j++)
 	            {
 	                if(j==0)
 	                    dp[i][j] = dp[i-1][j] + triangle[i][j];
 	                
 	                else if(j==i)
-	                	dp[i][j] = dp[i-1][j-1] + triangle[i][j];
+	                    dp[i][j] = dp[i-1][j-1] + triangle[i][j];
 	                
 	                else
 	                    dp[i][j] = Math.max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j];
@@ -26,11 +29,13 @@ public class Solution {
 	        int dl = dp.length-1;
 	        int dll = dp[dl].length;
             
-            int max = -9;
+            	int max = -9;
+
+		// dp에 저장된 값 중 최대값 구하기
 	        for(int i=0; i<dll; i++)
 	            max = max < dp[dl][i] ? dp[dl][i] : max;
 	        
-            answer = max;
+            	answer = max;
             
 	        return answer;
 	    }
