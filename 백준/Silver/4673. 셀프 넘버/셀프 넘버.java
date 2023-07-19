d96e771066e0e0ce57ecef11f199543f4ba12827
huni19541 <133645725+huni19541@users.noreply.github.com>
2023-07-20 08:46:15 +0900

    Update 셀프 넘버.java

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -2,29 +2,31 @@
 public class Main {
     
 	public static void main(String[] args) {
-		
 		selfNumber(10000);
 	}
-	
+
+	// n까지 셀프넘버 구하는 함수
 	static void selfNumber(int n)
 	{
-		
 		for(int i=1; i<=n; i++)
 		{
 			boolean check = true;
 			for(int j=1; j<i; j++)
 			{
+				// 생성자가 있다면, break;
 				if(d(j) == i)
 				{
 					check = false;
 					break;
 				}
 			}
+			// 생성자가 없다면 print
 			if(check)
 				System.out.println(i);
 		}
 	}
-	
+
+	// 생성자 구하는 함수
 	static int d(int n)
 	{
 		int sum = n;
