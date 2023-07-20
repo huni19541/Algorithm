23b63da0a11bfc9156359cded76ac5783dbc27fb
huni19541 <133645725+huni19541@users.noreply.github.com>
2023-07-20 09:57:26 +0900

    Update 유기농 배추.java

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
 
+	// 변수 선언
 	static int T, M, N, K, X, Y;
 	static int[][] map;
 	static boolean[][] check;
@@ -10,9 +11,11 @@ public class Main {
 	final static int[] dr = {0, 0, -1, 1}; // 상 하 좌 우
 	
 	public static void main(String[] args) {
-		
+
+		// 입력
 		Scanner sc = new Scanner(System.in);
 		T = sc.nextInt();
+		
 		for(int test_case=0; test_case<T; test_case++)
 		{
 			M = sc.nextInt();
@@ -30,15 +33,13 @@ public class Main {
 				map[Y][X] = 1;
 			}
 			
-			
-			
 			countWarm();
 		}
 		
-		
 		sc.close();
 	}
-	
+
+	// 지렁이 개수 count
 	static void countWarm()
 	{
 		int cnt = 0;
@@ -46,9 +47,11 @@ public class Main {
 		{
 			for(int j=0; j<M; j++)
 			{
+				// 배추가 없거나, 이미 확인 한 경우
 				if(map[i][j] == 0 || check[i][j] == true)
 					continue;
-				
+
+				// 배추가 있고, 확인 안한 경우
 				if(map[i][j] == 1 && check[i][j] == false)
 				{
 					check[i][j] = true;
@@ -59,9 +62,11 @@ public class Main {
 		}
 		System.out.println(cnt);
 	}
-	
+
+	// 배추 찾기
 	static void find(int col, int row)
 	{
+		// 4 방향 찾기
 		for(int k=0; k<4; k++)
 		{
 			int nc = col + dc[k];
@@ -69,6 +74,7 @@ public class Main {
 			
 			if(nc >= 0 && nc < N && nr >= 0 && nr < M)
 			{
+				// 배추가 있고, 확인 안한 경우 재귀함수 호출
 				if(!check[nc][nr] && map[nc][nr] == 1)
 				{
 					check[nc][nr] = true;
