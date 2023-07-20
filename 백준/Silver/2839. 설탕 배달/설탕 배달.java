73c16a2610c266e53718371b032c4547798415ce
huni19541 <133645725+huni19541@users.noreply.github.com>
2023-07-20 16:02:22 +0900

    Update 설탕 배달.java

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -10,37 +10,44 @@ public class Main {
 		N = sc.nextInt();
 
 		int[] dp = new int[N + 1];
-		dp[1] = -1;
-		dp[2] = -1;
-		dp[3] = 1;
-		
-		if(N == 4)
-			dp[4] = -1;
-		
-		if(N >= 5)
-		{
-			dp[4] = -1;
-			dp[5] = 1;
-		}
 
-		for (int i = 6; i <= N; i++) {
+		// 초기값 설정
+		for (int i = 1; i <= N; i++) {
 			
+			if (i - 5 <= 0)
+			{
+				if(i == 3)
+					dp[i] = 1;
+				
+				else if (i == 5)
+					dp[i] = 1;
+				
+				else
+					dp[i] = -1;
+				
+				continue;
+			}
+
+			// 3킬로, 5킬로 둘 다 가능한 경우
 			if (dp[i - 3] > 0 && dp[i - 5] > 0)
 				dp[i] = Math.min(dp[i - 3] + 1, dp[i - 5] + 1);
-			
-			else if(dp[i-3] > 0)
-				dp[i] = dp[i-3] + 1;
 
-			else if(dp[i-5] > 0)
-				dp[i] = dp[i-5] + 1;
-			
+			// 3킬로만 가능한 경우
+			else if (dp[i - 3] > 0)
+				dp[i] = dp[i - 3] + 1;
+
+			// 5킬로만 가능한 경우
+			else if (dp[i - 5] > 0)
+				dp[i] = dp[i - 5] + 1;
+
+			// 만들 수 없는 경우
 			else
 				dp[i] = -1;
 		}
 
 		int res = dp[N];
 		System.out.println(res);
-		
+
 		sc.close();
 
 	}
