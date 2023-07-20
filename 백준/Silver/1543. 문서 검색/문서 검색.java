2122a3100f2c2bcab9235b116daac1fecd0be297
huni19541 <133645725+huni19541@users.noreply.github.com>
2023-07-20 12:54:37 +0900

    Update 문서 검색.java

--- 백준/Silver/1543. 문서 검색/문서 검색.java
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -2,12 +2,15 @@ import java.util.Scanner;
 
 public class Main {
 
+	// 변수 선언
 	static String doc;
 	static String word;
 	
 	public static void main(String[] args)
 	{
 		Scanner sc = new Scanner(System.in);
+
+		// space도 포함 -> next() 대신 nextLine()
 		doc = sc.nextLine();
 		word = sc.nextLine();
 		
@@ -16,15 +19,18 @@ public class Main {
 		
 		sc.close();
 	}
-	
+
+	// 중복 검색 횟수 반환해주는 함수
 	static int find(String doc, String word)
 	{
 		int cnt = 0;
-		
+
+		// word크기 만큼 남았을 때까지 for문
 		for(int i=0; i<=doc.length()-word.length();)
 		{
 			boolean check = true;
-			
+
+			// 한 문자씩 비교
 			for(int j=0; j<word.length(); j++)
 			{
 				if(doc.charAt(i+j) != word.charAt(j))
@@ -33,12 +39,15 @@ public class Main {
 					break;
 				}
 			}
-			
+
+			// 일치하면 word 길이 만큼 idx늘리기
 			if(check == true)
 			{
 				cnt++;
 				i += word.length();
 			}
+
+			// 불일치 -> idx++
 			else
 				i++;
 		}
