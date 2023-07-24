2dcf022d7bdfba45d5b1bdbc9c8f6d16fb906cbe
huni19541 <133645725+huni19541@users.noreply.github.com>
2023-07-24 16:58:11 +0900

    [level 2] Title: 의상, Time: 0.06 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.java
+++ 프로그래머스/lv2/42578. 의상/의상.java
@@ -2,21 +2,17 @@ import java.util.HashMap;
 import java.util.Map.Entry;
 
 class Solution {
-    public int solution(String[][] clothes)
-	{
-		int answer = 1;
-		
+    public int solution(String[][] clothes) {
+		int cnt = 1;
 		HashMap<String, Integer> map = new HashMap<>();
-		
-		for(int i=0; i<clothes.length; i++)
+
+		for (int i = 0; i < clothes.length; i++)
 			map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
-		
-		
-		for(Entry<String, Integer> entryset : map.entrySet())
-			answer *= (entryset.getValue() + 1);
-		
-		answer -= 1;
-		
-		return answer;
+
+		for (Entry<String, Integer> entryset : map.entrySet())
+			cnt *= (entryset.getValue() + 1);
+
+		cnt -= 1;
+		return cnt;
 	}
 }
