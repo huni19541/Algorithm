6dfc4bffc4a22a0dc6044a942d7c698e735fa710
huni19541 <133645725+huni19541@users.noreply.github.com>
2023-07-24 12:51:14 +0900

    Update 수들의 합 2.java

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -17,18 +17,21 @@ public class Main {
 		int res = count(A, M);
 		System.out.println(res);
 	}
-	
+
+	// 누적합 계산
 	static int count(int[] arr, int M)
 	{
 		int cnt = 0;
 		for(int i=0; i<arr.length; i++)
 		{
 			int sum = 0;
+			// i부터 누적합이 M 넘으면 break;
 			for(int j=i; j<arr.length; j++)
 			{
 				sum += arr[j];
 				if(sum >= M) break;
 			}
+			// 누적합이 M 이면 cnt++
 			if(sum == M) cnt++;
 		}
 		return cnt;
