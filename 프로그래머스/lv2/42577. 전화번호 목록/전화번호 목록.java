d679647c34c288ea582c4bce6428a15d4db2489c
huni19541 <133645725+huni19541@users.noreply.github.com>
2023-07-24 16:47:16 +0900

    [level 2] Title: 전화번호 목록, Time: 278.08 ms, Memory: 97.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,20 +1,31 @@
 import java.util.HashMap;
+import java.util.Arrays;
 
 class Solution {
-    public boolean solution(String[] phone_book) {
-		HashMap<String, Integer> map = new HashMap<>();
-
-		int len = phone_book.length;
-
-		for (int i = 0; i < len; i++)
-			map.put(phone_book[i], i);
+  public boolean solution(String[] phone_book) {
 		
-		for(int i=0; i<len; i++)
-		{
-			int slen = phone_book[i].length();
-			for(int j=0; j<slen; j++)
-			{
-				if(map.containsKey(phone_book[i].substring(0, j)))
+		Arrays.sort(phone_book);
+		
+		for(int i = 0; i< phone_book.length - 1; i++) {
+			for(int j = i + 1; j < phone_book.length; j++) {
+				if(phone_book[j].startsWith(phone_book[i]))
+					return false;
+                else
+                    break;
+			}
+		}
+		return true;
+	}
+	
+	public boolean solution2(String[] phone_book) {
+		
+		HashMap<String, Integer> map = new HashMap<>();
+		for(String s : phone_book)
+			map.put(s, map.getOrDefault(s, 0) + 1);
+		
+		for(String s : map.keySet()) {
+			for(int i=0; i<s.length(); i++) {
+				if(map.containsKey(s.substring(0, i)))
 					return false;
 			}
 		}
