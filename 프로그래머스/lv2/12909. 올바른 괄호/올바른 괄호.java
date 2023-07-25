fe8b47f60d4e52a536890e532950be27c2a1f70a
huni19541 <133645725+huni19541@users.noreply.github.com>
2023-07-25 09:59:05 +0900

    [level 2] Title: 올바른 괄호, Time: 19.20 ms, Memory: 52.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,30 +1,31 @@
 import java.util.Stack;
 
 class Solution {
-    boolean solution(String s) {
-        boolean answer = true;
-        
-        Stack<Character> stack = new Stack<>();
-        char c;
-        
-        for(int i=0; i<s.length(); i++)
-        {
-            c = s.charAt(i);
-            if(c == '(')
-                stack.push(c);
-            else
-            {
-                if(stack.empty())
-                {
-                    answer = false;
-                    break;
-                }
-                stack.pop();
-            }
-        }
-        if(!stack.empty())
-            answer = false;
-
-        return answer;
-    }
+    public static boolean solution(String s) {
+		boolean answer = true;
+		Stack<Character> stack = new Stack<>();
+		
+		for(int i=0; i<s.length(); i++) {
+			char c = s.charAt(i);
+			if(c == '(')
+				stack.push(c);
+			
+			else if(c == ')') {
+				if(stack.isEmpty()) {
+					answer = false;
+					break;
+				}
+				
+				if(stack.peek() == '(') {
+					stack.pop();
+				}
+			}
+		}
+		
+		if(!stack.isEmpty()) {
+			answer = false;
+		}
+		
+		return answer;
+	}
 }
