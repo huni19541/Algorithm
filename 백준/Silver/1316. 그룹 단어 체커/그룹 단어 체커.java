eb8daf3008436ebe305b1797be45d290445a5941
huni19541 <133645725+huni19541@users.noreply.github.com>
2023-07-26 17:35:31 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 132 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,19 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		sc.nextLine();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
 		String str = "";
 		
 		int cnt = 0;
 		for(int i=0; i<N; i++) {
 			HashMap<Character, Integer> map = new HashMap<>();
-			str = sc.nextLine();
+			str = br.readLine();
 			boolean check = true;
 			for(int j=0; j<str.length(); j++) {
 				char c = str.charAt(j);
