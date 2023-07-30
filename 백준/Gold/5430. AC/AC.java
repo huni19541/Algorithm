0e0a3bc74597d645dc630fdd863fcd95cb1d0af6
huni19541 <133645725+huni19541@users.noreply.github.com>
2023-07-30 21:41:56 +0900

    [Gold V] Title: AC, Time: 852 ms, Memory: 89052 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -6,74 +6,79 @@ import java.util.Deque;
 
 public class Main {
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static Deque<Integer> deque = new ArrayDeque<>();
+	
+	public static void main(String[] args) throws IOException {
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
 		
-		outer:
 		for(int i=0; i<T; i++) {
-			boolean reverse = false;
 			sb.setLength(0);
+			deque.clear();
 			
-			Deque<Integer> deque = new ArrayDeque<>();
-			String p = br.readLine();
-			int pLen = p.length();
-			int n = Integer.parseInt(br.readLine());
-			String temp = br.readLine();
-			
-			if(n != 0) {
-				String[] sArr = temp.substring(1, temp.length()-1).split(",");
-				
-				for(String s : sArr) {
-					deque.add(Integer.valueOf(s));
-				}
-			}
-			
-			
-			for(int j=0; j<pLen; j++) {
-				char c = p.charAt(j);
-				
-				if(c == 'R') {
-					reverse = (reverse == true) ? false : true;
-				} else if(c == 'D') {
-					if(deque.size() == 0) {
-						System.out.println("error");
-						continue outer;
-					}
-					
-					if(!reverse) {
-						deque.pollFirst();
-						n--;
-					} else {
-						deque.pollLast();
-						n--;
-					}
-				}
-			}
-			
-			if(n == 0) {
-				System.out.println("[]");
-				continue;
-			}
-		
-			sb.append('[');
-			if(!reverse) {
-				sb.append(deque.pollFirst());
-				while(!deque.isEmpty()) {
-					sb.append(',').append(deque.pollFirst());
-				}
-				
-			} else {
-				sb.append(deque.pollLast());
-				while(!deque.isEmpty()) {
-					sb.append(',').append(deque.pollLast());
-				}
-			}
-			sb.append(']');
-			System.out.println(sb);
+			AC();
 		}
 	}
-
+	
+	static void AC() throws IOException {
+		boolean reverse = false;
+		
+		String p = br.readLine();
+		int pLen = p.length();
+		int n = Integer.parseInt(br.readLine());
+		String temp = br.readLine();
+		
+		if(n != 0) {
+			String[] sArr = temp.substring(1, temp.length()-1).split(",");
+			
+			for(String s : sArr) {
+				deque.add(Integer.valueOf(s));
+			}
+		}
+		
+			
+		for(int i=0; i<pLen; i++) {
+			char c = p.charAt(i);
+			
+			if(c == 'R') {
+				reverse = (reverse == true) ? false : true;
+			} else if(c == 'D') {
+				if(deque.size() == 0) {
+					System.out.println("error");
+					return;
+				}
+				
+				if(!reverse) {
+					deque.pollFirst();
+					n--;
+				} else {
+					deque.pollLast();
+					n--;
+				}
+			}
+		}
+		
+		if(n == 0) {
+			System.out.println("[]");
+			return;
+		}
+	
+		sb.append('[');
+		if(!reverse) {
+			sb.append(deque.pollFirst());
+			while(!deque.isEmpty()) {
+				sb.append(',').append(deque.pollFirst());
+			}
+			
+		} else {
+			sb.append(deque.pollLast());
+			while(!deque.isEmpty()) {
+				sb.append(',').append(deque.pollLast());
+			}
+		}
+		sb.append(']');
+		System.out.println(sb);
+	}
 }
