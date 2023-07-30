1823c59e019bf994fb92ffc69b8b2219d8ccd5ac
huni19541 <133645725+huni19541@users.noreply.github.com>
2023-07-30 22:27:09 +0900

    [Silver I] Title: 절댓값 힙, Time: 324 ms, Memory: 25808 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,30 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> {
-				int res1 = Math.abs(o1.intValue());
-				int res2 = Math.abs(o2.intValue());
-				return ((res1 - res2) == 0) ? (o1.intValue() - o2.intValue()) : res1 - res2;
-		}) ;
+	public static void main(String[] args) throws IOException {
+//		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		
-		int N = sc.nextInt();
-		for(int i=0; i<N; i++) {
-			int x = sc.nextInt();
-			if(x == 0) {
-				if(pq.isEmpty()) {
-					System.out.println(0);
+		PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> {
+			int res1 = Math.abs(o1.intValue());
+			int res2 = Math.abs(o2.intValue());
+			return ((res1 - res2) == 0) ? (o1.intValue() - o2.intValue()) : res1 - res2;
+		});
+
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++) {
+			int x = Integer.parseInt(br.readLine());
+			if (x == 0) {
+				if (pq.isEmpty()) {
+					sb.append(0).append('\n');
 					continue;
 				} else {
-					System.out.println(pq.poll());
+					sb.append(pq.poll()).append('\n');
 				}
 			} else {
 				pq.add(x);
 			}
 		}
+
+		System.out.println(sb);
 	}
-	
+
 }
