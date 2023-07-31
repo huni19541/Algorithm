f10b814fcfe2b49f55428387959b46da1945dab4
huni19541 <133645725+huni19541@users.noreply.github.com>
2023-07-31 14:08:27 +0900

    Update 타겟 넘버.java

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,22 +1,25 @@
 class Solution {
-    static int[] nums;
+    	static int[] nums;
 	static boolean[] visit;
 	static int cnt = 0;
 	
 	public static int solution(int[] numbers, int target) {
-        int answer = 0;
-        nums = numbers;
-        visit = new boolean[nums.length];
-        
-        for(int i=0; i<nums.length; i++) {
-        	iter(0, i, target);
-        }
-        
-        answer = cnt;
-        return answer;
-    }
-	
+	        int answer = 0;
+	        nums = numbers;
+	        visit = new boolean[nums.length];
+
+		// 음수가 없는 것 부터 전부 음수인 것 까지 재귀
+	        for(int i=0; i<nums.length; i++) {
+	        	iter(0, i, target);
+	        }
+	        
+	        answer = cnt;
+	        return answer;
+    	}
+
+	// 타겟넘버 구하기
 	public static void iter(int idx, int minusCnt, int target) {
+		// 음수를 정했으면 타겟넘버와 비교
 		if(minusCnt == 0) {
 			int sum = 0;
 			for(int i=0; i<nums.length; i++) {
@@ -29,7 +32,8 @@ class Solution {
 			}
 			if(sum == target) cnt++;
 		}
-		
+
+		// 완전탐색
 		for(int i=idx; i<nums.length; i++) {
 			visit[i] = true;
 			iter(i+1, minusCnt-1, target);
