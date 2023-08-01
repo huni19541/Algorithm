0115179df07346ac1a79359b02784875ebd97d00
huni19541 <133645725+huni19541@users.noreply.github.com>
2023-08-02 08:43:02 +0900

    [Bronze V] Title: 꼬마 정민, Time: 124 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		long A = Long.parseLong(st.nextToken());
+		long B = Long.parseLong(st.nextToken());
+		long C = Long.parseLong(st.nextToken());
+		
+		long sum = A + B + C;
+		sb.append(sum).append('\n');
+		
+		System.out.println(sb);
+	}
+
+}
