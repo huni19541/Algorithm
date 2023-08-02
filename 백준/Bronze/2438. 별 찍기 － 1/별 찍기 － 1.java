7e79cc05699385b9d593976297dd68f0a2441b6a
huni19541 <133645725+huni19541@users.noreply.github.com>
2023-08-02 10:30:18 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 136 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<=i; j++) {
+				sb.append("*");
+			}
+			sb.append('\n');
+		}
+		
+		System.out.println(sb);
+	}
+
+}
