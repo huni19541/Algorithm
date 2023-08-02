d79db664b7ff88498041024977eebc77a4e181c6
huni19541 <133645725+huni19541@users.noreply.github.com>
2023-08-02 17:19:35 +0900

    [Bronze V] Title: 새싹, Time: 124 ms, Memory: 14016 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+	static int res = 0;
+	static int N, M;
+	
+	public static void main(String[] args) throws IOException {
+		
+		//System.setIn(new FileInputStream("Test.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+//		int T = Integer.parseInt(br.readLine());
+		
+		sb.append("         ,r'\"7\n");
+		sb.append("r`-_   ,'  ,/\n");
+		sb.append(" \\. \". L_r'\n");
+		sb.append("   `~\\/\n");
+		sb.append("      |\n");
+		sb.append("      |\n");
+		
+		System.out.println(sb);
+		
+	}
+	
+}
