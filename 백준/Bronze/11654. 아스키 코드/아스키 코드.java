2762ae454e99adbb286cb30e9eea6bd60adbf84c
huni19541 <133645725+huni19541@users.noreply.github.com>
2023-08-03 10:29:50 +0900

    [Bronze V] Title: 아스키 코드, Time: 124 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+	static int res = 0;
+	static int N, M;
+	
+	public static void main(String[] args) throws IOException {
+		
+		//System.setIn(new FileInputStream("Test.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		char c = br.readLine().toCharArray()[0];
+		
+		sb.append((int)c);
+		
+		
+		
+		System.out.println(sb);
+	}
+	
+}
