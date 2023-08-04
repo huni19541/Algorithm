8a5f2d11368b6fb00747812ed8281f04b584e09b
huni19541 <133645725+huni19541@users.noreply.github.com>
2023-08-04 15:09:55 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 668 ms, Memory: 50932 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		
+		//System.setIn(new FileInputStream("Test.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		int[][] arr = new int[N][2];
+		
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr, (o1, o2)->{
+			int o1Res = o1[0];
+			int o2Res = o2[0];
+			
+			return (o1Res == o2Res) ? o1[1] - o2[1] : o1Res - o2Res;
+		});
+		
+		for(int[] a : arr) {
+			sb.append(a[0]).append(" ").append(a[1]).append('\n');
+		}
+		
+		
+		System.out.println(sb);
+	}
+		
+}
