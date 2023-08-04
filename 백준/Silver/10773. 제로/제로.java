7d32161c05d9cf871441d471733f7ad64b727ccf
huni19541 <133645725+huni19541@users.noreply.github.com>
2023-08-04 16:00:01 +0900

    [Silver IV] Title: 제로, Time: 276 ms, Memory: 23736 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		
+		
+		//System.setIn(new FileInputStream("Test.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		Stack<Integer> stack = new Stack<>();
+		
+		for(int i=0; i<N; i++) {
+			int num = Integer.parseInt(br.readLine());
+			
+			if(num == 0) stack.pop();
+			else stack.push(num);
+		}
+		
+		int sum = 0;
+		while(!stack.isEmpty())
+			sum += stack.pop();
+		
+		sb.append(sum);
+		
+		System.out.println(sb);
+	}
+
+}
