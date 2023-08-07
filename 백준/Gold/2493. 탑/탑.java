389d39d952294076a275fe84cfe4e64ae195afdf
huni19541 <133645725+huni19541@users.noreply.github.com>
2023-08-07 15:44:17 +0900

    Update 탑.java

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,46 +1,45 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-public class Main {
+public class B2493_김동훈 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
 		
 		int N = Integer.parseInt(br.readLine());
-		int[] resArr = new int[N+1];
-		int idx = N-1;
 		
-		String[] nums = br.readLine().split(" ");
+		st = new StringTokenizer(br.readLine());
+		
+		// 기둥의 높이와 번호 저장
+		int[][] nums = new int[N+1][2];
+		for(int i=1; i<=N; i++) {
+			nums[i][0] = Integer.parseInt(st.nextToken());
+			nums[i][1] = i;
+		}
+		
+		// 결과 저장 배열
+		int[] resArr = new int[N+1];
 		
 		Stack<int[]> stack = new Stack<>();
-		for(int i=0; i<N; i++) {
+		stack.push(nums[N]);
+		int[] num;
+		for(int i=N-1; i>=1; i--) {
+			// 현재 값과 기둥 번호
+			num = nums[i];
 			
-			int[] num = new int[] {Integer.parseInt(nums[N-1-i]), N-i};
-			
-			if(stack.isEmpty()) {
-				stack.push(num);
-				continue;
-			}
-			
+			// 현재 기둥이 이전 기둥보다 크다면
 			while(stack.peek()[0] < num[0]) {
-				resArr[stack.pop()[1]] = N-i;
+				resArr[stack.pop()[1]] = i;
 				if(stack.isEmpty()) break;
 			}
 			
 			stack.push(num);
-		}
-		
-		while(!stack.isEmpty())
-		{
-			resArr[stack.pop()[1]] = 0;
 		}
 
 		for(int i=1; i<=N; i++)
