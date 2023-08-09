4d364c743b4d975cbc52d34c86f69c87ea9a0dd0
huni19541 <133645725+huni19541@users.noreply.github.com>
2023-08-09 14:46:33 +0900

    [Silver I] Title: 절댓값 힙, Time: 324 ms, Memory: 25808 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -7,44 +7,31 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		// Scanner 사용 시 BufferedReader 보다 메모리 4배, 시간 3배 증가 
 //		Scanner sc = new Scanner(System.in);
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		// StringBuilder 사용해서 출력문 만드는 것 -> 시간 단축
 		StringBuilder sb = new StringBuilder();
-
-		// PriorityQueue 정렬 방법을 람다식으로 재정의
+		
 		PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> {
 			int res1 = Math.abs(o1.intValue());
 			int res2 = Math.abs(o2.intValue());
 			return ((res1 - res2) == 0) ? (o1.intValue() - o2.intValue()) : res1 - res2;
 		});
 
-//		int N = sc.nextInt();
 		int N = Integer.parseInt(br.readLine());
 		for (int i = 0; i < N; i++) {
-//			int x = sc.nextInt();
 			int x = Integer.parseInt(br.readLine());
-
-			// 입력이 0일 때
 			if (x == 0) {
-				// pr가 비어있으면 sb에 "0\n" 추가
 				if (pq.isEmpty()) {
 					sb.append(0).append('\n');
 					continue;
-				} 
-				// pr에 값이 있으면 sb에 맨 위의 값 + '\n' 추가
-				else {
+				} else {
 					sb.append(pq.poll()).append('\n');
 				}
-			} 
-			// 입력이 0이 아닐 때 pq에 값 추가
-			else {
+			} else {
 				pq.add(x);
 			}
 		}
-		// sb 출력
+
 		System.out.println(sb);
 	}
 
