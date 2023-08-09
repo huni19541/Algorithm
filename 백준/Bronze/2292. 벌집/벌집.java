cbedeb8f572e3463d2372b9b49712d56afc71e1c
huni19541 <133645725+huni19541@users.noreply.github.com>
2023-08-09 14:27:28 +0900

    [Bronze II] Title: 벌집, Time: 124 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		
+		//System.setIn(new FileInputStream("Test.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		N--;
+		
+		int cnt = 1;
+		while(N > 0) {
+			N -= 6*(cnt++);
+		}
+		
+		sb.append(cnt);
+		System.out.println(sb);
+	}
+
+}
