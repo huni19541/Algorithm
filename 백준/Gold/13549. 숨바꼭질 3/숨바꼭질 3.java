8366bc625726eebd34cd78f600d3784eab28fdcb
huni19541 <133645725+huni19541@users.noreply.github.com>
2023-08-10 14:56:24 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 160 ms, Memory: 18900 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -0,0 +1,95 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+
+public class Main {
+	
+	static class Info{
+		int pos;
+		int depth;
+		int telpoCnt;
+		
+		Info(int pos, int depth, int telpoCnt){
+			this.pos = pos;
+			this.depth = depth;
+			this.telpoCnt = telpoCnt;
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		
+		
+		//System.setIn(new FileInputStream("Test.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		boolean[] visit = new boolean[100001];
+		
+		Queue<Info> queue = new LinkedList<>();
+		
+		queue.offer(new Info(N, 0, 0));
+		visit[N] = true;
+		
+		int curPos, curDepth, curTelpoCnt;
+		while(!queue.isEmpty()) {
+			Info cur = queue.poll();
+			
+			curPos = cur.pos;
+			curDepth = cur.depth;
+			curTelpoCnt = cur.telpoCnt;
+			
+			if(curPos == K) {
+				sb.append(curDepth-curTelpoCnt);
+				break;
+			}
+			
+			if(curPos <= 50000) {
+				if(!visit[curPos*2]) {
+					visit[curPos*2] = true;
+					queue.offer(new Info(curPos*2, curDepth+1, curTelpoCnt+1));
+				}
+			}
+			
+			if(curPos > 0) {
+				if(!visit[curPos-1]) {
+					visit[curPos-1] = true;
+					queue.offer(new Info(curPos-1, curDepth+1, curTelpoCnt));
+				}
+			}
+			
+			if(curPos < 100000) {
+				if(!visit[curPos+1]) {
+					visit[curPos+1] = true;
+					queue.offer(new Info(curPos+1, curDepth+1, curTelpoCnt));
+				}
+			}
+			
+			
+		}
+		
+		System.out.println(sb);
+	}
+		
+}
