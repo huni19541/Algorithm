e31647897c8c99efd23c50262bec1f3501fd0a33
huni19541 <133645725+huni19541@users.noreply.github.com>
2023-08-14 16:13:08 +0900

    [Gold V] Title: 괄호의 값, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.cc
@@ -0,0 +1,76 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+string str;
+stack<char> s;
+
+int main(int argc, char* argv[])
+{
+	cin >> str;
+
+	int answer = 0, temp = 1;
+
+	for (int i = 0; i < str.length(); i++)
+	{
+		if (str[i] == '(')
+		{
+			temp *= 2;
+			s.push('(');
+		}
+		else if (str[i] == '[')
+		{
+			temp *= 3;
+			s.push('[');
+		}
+		else if (str[i] == ')')
+		{
+			if (s.empty() || s.top() != '(')
+			{
+				answer = 0;
+				break;
+			}
+			if (str[i - 1] == '(')
+			{
+				answer += temp;
+				temp /= 2;
+				s.pop();
+			}
+			else
+			{
+				temp /= 2;
+				s.pop();
+			}
+		}
+		else if (str[i] == ']')
+		{
+			if (s.empty() || s.top() != '[')
+			{
+				answer = 0;
+				break;
+			}
+			if (str[i - 1] == '[')
+			{
+				answer += temp;
+				temp /= 3;
+				s.pop();
+			}
+			else
+			{
+				temp /= 3;
+				s.pop();
+			}
+		}
+	}
+
+	if (!s.empty()) answer = 0;
+
+	printf("%d", answer);
+}
+
