b30cec7660d34267ee0287093d519c1c53d74fa3
huni19541 <133645725+huni19541@users.noreply.github.com>
2023-08-20 19:07:37 +0900

    [Silver I] Title: 미로 탐색, Time: 192 ms, Memory: 16612 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,22 +1,5 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map.Entry;
-import java.util.Queue;
-import java.util.Set;
-import java.util.Stack;
-import java.util.StringTokenizer;
-
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
@@ -31,6 +14,7 @@ public class Main {
 			this.depth = depth;
 		}
 	}
+	
 	static int N, M;
 	static int[][] map;
 	static boolean[][] visit;
@@ -59,7 +43,7 @@ public class Main {
 			}
 		}
 		
-		Queue<Info> queue = new LinkedList<>();
+		Queue<Info> queue = new ArrayDeque<>();
 		queue.add(new Info(0, 0, 1));
 		while(!(queue.peek().y == N-1 && queue.peek().x == M-1)) {
 			Info cur = queue.poll();
