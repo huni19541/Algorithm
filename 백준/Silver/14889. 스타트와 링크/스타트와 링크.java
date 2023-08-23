98208601d1cb9d18a080ad56f809140fc70cfc2c
huni19541 <133645725+huni19541@users.noreply.github.com>
2023-08-23 17:53:15 +0900

    [Silver II] Title: 스타트와 링크, Time: 528 ms, Memory: 19988 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,77 +1,71 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 	
-	static int N;
-	static int[][] S;
-	static boolean[] visit; 
-	static int min = Integer.MAX_VALUE;
-	
-	public static void main(String[] args)
-	{
-		Scanner sc = new Scanner(System.in);
-		
-		N = sc.nextInt();
-		
-		S = new int[N][N];
-		visit = new boolean[N];
-		
-		for(int i=0; i<N; i++)
-			for(int j=0; j<N; j++)
-				S[i][j] = sc.nextInt();
-		
-		iter(0, 0);
-		System.out.println(min);
-		
-	}
-	
-	static void iter(int idx, int count)
-	{
-		if(count == N/2)
-		{
-			int res = calc();
-			min = min > res ? res : min;
-			if(min == 0)
-			{
-				System.out.println(min);
-				System.exit(0);
-			}
-			return;
-		}
-		
-		for(int i=idx; i<N; i++)
-		{
-			visit[i] = true;
-			iter(i+1, count+1);
-			visit[i] = false;
-		}
-	}
-	
-	static int calc()
-	{
-		int start = 0, link = 0;
-		
-		for(int i=0; i<N-1; i++)
-		{
-			for(int j=i+1; j<N; j++)
-			{
-				if(visit[i] == true && visit[j] == true)
-				{
-					start += S[i][j];
-					start += S[j][i];
-				}
-				
-				if(visit[i] == false && visit[j] == false)
-				{
-					link += S[i][j];
-					link += S[j][i];
-				}
-			}
-		}
-		
-		
-		int res = Math.abs(Math.subtractExact(start, link));
-		return res;
-	}
 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		int N = Integer.parseInt(br.readLine());
+		
+		int[][] map = new int[N][N];
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		int[] flag = new int[N];
+		for(int i=N/2; i<N; i++)
+			flag[i] = 1;
+		
+		int res = Integer.MAX_VALUE;
+		do {
+			int sum1 = 0;
+			int sum2 = 0;
+			
+			for(int i=0; i<N; i++) {
+				for(int j=0; j<N; j++) {
+					if(i==j) continue;
+					if(flag[i] == 1 && flag[j] == 1)
+						sum1 += map[i][j];
+					if(flag[i] == 0 && flag[j] == 0)
+						sum2 += map[i][j];
+				}
+			}
+			
+			res = Math.min(res, Math.abs(sum1-sum2));
+			
+		} while(NP(flag));
+
+		sb.append(res);
+		System.out.println(sb);
+	}
+	
+	private static boolean NP(int[] p) {
+		int N = p.length;
+		
+		int i = N-1;
+		while(i > 0 && p[i-1] >= p[i]) i--;
+		if(i == 0) return false;
+		
+		int j = N-1;
+		while(p[i-1] >= p[j]) j--;
+		swap(p, i-1, j);
+		
+		int k = N-1;
+		while(i < k) swap(p, i++, k--);
+		
+		return true;
+	}
+	
+	private static void swap(int[] p, int a, int b) {
+		int temp = p[a];
+		p[a] = p[b];
+		p[b] = temp;
+	}
 }
