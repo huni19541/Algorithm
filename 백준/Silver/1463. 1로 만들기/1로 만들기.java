fe73b600c0d773d6738413f5ef65ca359f53bcf5
huni19541 <133645725+huni19541@users.noreply.github.com>
2023-08-29 14:45:23 +0900

    [Silver III] Title: 1로 만들기, Time: 144 ms, Memory: 18024 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,50 +1,28 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map.Entry;
-import java.util.Queue;
-import java.util.Stack;
-import java.util.StringTokenizer;
-
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	static int res = 0;
-	static int N, M;
-	
 	public static void main(String[] args) throws IOException {
-		
-		//System.setIn(new FileInputStream("Test.txt"));
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
 		
-		int T = Integer.parseInt(br.readLine());
-		int[] dp = new int[T+1];
+		int N = Integer.parseInt(br.readLine());
+		int[] dp = new int[N+1];
 		
-		for(int i=2; i<=T; i++) {
-			
+		for(int i=2; i<=N; i++) {
 			dp[i] = dp[i-1] + 1;
-			
 			if(i % 3 == 0) {
-				dp[i] = Math.min(dp[i], dp[i/3]+1);
+				dp[i] = Math.min(dp[i], dp[i/3] + 1);
 			}
 			if(i % 2 == 0) {
-				dp[i] = Math.min(dp[i], dp[i/2]+1);
+				dp[i] = Math.min(dp[i], dp[i/2] + 1);
 			}
 		}
 		
-		sb.append(dp[T]);
-		
+		sb.append(dp[N]);
 		System.out.println(sb);
-		
 	}
-	
+
 }
