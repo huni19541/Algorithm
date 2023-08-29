f51dc545cd347e5fa3227b947912cd56bbecdb93
huni19541 <133645725+huni19541@users.noreply.github.com>
2023-08-29 15:09:51 +0900

    [Silver I] Title: RGB거리, Time: 132 ms, Memory: 15232 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -32,12 +32,10 @@ public class Main {
 		
 		for(int i=0; i<N; i++) {
 			for(int j=0; j<3; j++) {
-				// dp 초기값 설정
 				if(i == 0) {
 					dp[i][j] = arr[i][j];
 					continue;
 				}
-				// 다른 색 중에서 작은 값으로 dp 저장
 				if(j == 0) {
 					dp[i][j] = arr[i][j] + Math.min(dp[i-1][j+1], dp[i-1][j+2]);
 				} else if(j == 1) {
@@ -47,8 +45,7 @@ public class Main {
 				}
 			}
 		}
-
-		// 맨 밑에 저장된 dp값들 중에서 최소값 출력
+		
 		for(int i=0; i<3; i++) {
 			min = dp[N-1][i] < min ? dp[N-1][i] : min;
 		}
