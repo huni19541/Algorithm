59022d0d522497c5d1fe929ceebbf3efa4f0f6d0
huni19541 <133645725+huni19541@users.noreply.github.com>
2023-09-05 12:19:10 +0900

    Update 케빈 베이컨의 6단계 법칙.java

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -6,9 +6,19 @@ public class Main {
 	static int N, M;
 	static int[][] dist;
 	static boolean[] visited;
-	static ArrayList<Integer>[] list;
+	static ArrayList<Node>[] list;
+    
+    static class Node {
+        int vertex;
+        int cost;
+        
+        public Node(int vertex, int cost){
+            this.vertex = vertex;
+            this.cost = cost;
+        }
+    }
 	
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
@@ -20,7 +30,7 @@ public class Main {
 		
 		list = new ArrayList[N+1];
 		for(int i=1; i<=N; i++) {
-			list[i] = new ArrayList<>();
+			list[i] = new ArrayList<Node>();
 		}
 		
 		for(int i=0; i<M; i++) {
@@ -28,8 +38,8 @@ public class Main {
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			
-			list[a].add(b);
-			list[b].add(a);
+			list[a].add(new Node(b, 1));
+			list[b].add(new Node(a, 1));
 		}
 		
 		dist = new int[N+1][N+1];
@@ -50,7 +60,7 @@ public class Main {
 			for(int j=1; j<=N; j++) {
 				sum += dist[i][j];
 			}
-			if(sum < min) {
+			if(min > sum) {
 				min = sum;
 				minIdx = i;
 			}
@@ -63,22 +73,23 @@ public class Main {
 	}
 	
 	static void dijkstra(int start) {
-		Queue<Integer> q = new ArrayDeque<>();
+		PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1.cost, o2.cost));
 		visited = new boolean[N+1];
 		
 		dist[start][start] = 0;
-		q.offer(start);
+		pq.offer(new Node(start, 0));
 		
-		while(!q.isEmpty()) {
-			int cur = q.poll();
+		while(!pq.isEmpty()) {
+			Node cur = pq.poll();
+			int now = cur.vertex;
+            
+			if(visited[now]) continue;
+			visited[now] = true;
 			
-			if(visited[cur]) continue;
-			visited[cur] = true;
-			
-			for(int next : list[cur]) {
-				if(dist[start][next] > dist[start][cur] + 1) {
-					dist[start][next] = dist[start][cur] + 1;
-					q.offer(next);
+			for(Node next : list[now]) {
+				if(dist[start][next.vertex] > dist[start][now] + 1) {
+					dist[start][next.vertex] = dist[start][now] + 1;
+					pq.offer(new Node(next.vertex, dist[start][next.vertex]));
 				}
 			}
 		}
