de999eb22904013b974b080641d6bd026736587d
huni19541 <133645725+huni19541@users.noreply.github.com>
2023-09-12 15:45:37 +0900

    [Gold V] Title: 빗물, Time: 136 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static class info {
+		int height;
+		int idx;
+		
+		public info(int height, int idx) {
+			this.height = height;
+			this.idx = idx;
+		}
+	}
+	
+	
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		st = new StringTokenizer(br.readLine());
+		int H = Integer.parseInt(st.nextToken());
+		int W = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[W];
+		
+		st = new StringTokenizer(br.readLine());
+		
+		
+		for(int i=0; i<W; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		int sum = 0;
+		for(int i=2; i<W; i++) {
+			for(int j=arr[i]; j>=1; j--) {
+				for(int k=i-1; k>=0; k--) {
+					if(arr[k] >= j) {
+						sum += (i-k-1);
+						break;
+					}
+				}
+			}
+		}
+		
+		sb.append(sum);
+		
+		
+		
+		System.out.println(sb);
+
+	}
+
+}
