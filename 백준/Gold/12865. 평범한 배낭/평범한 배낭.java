4173c55d7b292bb42912d5ce1a583044574ca899
huni19541 <133645725+huni19541@users.noreply.github.com>
2023-09-26 11:51:16 +0900

    [Gold V] Title: 평범한 배낭, Time: 196 ms, Memory: 53912 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,5 +1,22 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Queue;
+import java.util.Set;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
 
 public class Main {
 	
@@ -18,7 +35,7 @@ public class Main {
 		
 		int[] w = new int[N+1];
 		int[] v = new int[N+1];
-		int[] dp = new int[K+1];
+		int[][] dp = new int[N+1][K+1];
 		
 		for(int i=1; i<=N; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -28,16 +45,20 @@ public class Main {
 		}
 		
 		for(int i=1; i<=N; i++) {
-			for(int j=K; j>=0; j--) {
-					if(w[i] <= j) {
-					    dp[j] = Math.max(dp[j], dp[j-w[i]] + v[i]);
+			for(int j=1; j<=K; j++) {
+					if(j - w[i] >= 0) {
+					dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-w[i]] + v[i]);
+					}
+					
+					else {
+						dp[i][j] = dp[i-1][j];
 					}
 			}
 		}
 		
 		
 		
-		sb.append(dp[K]);
+		sb.append(dp[N][K]);
 		
 		System.out.println(sb);
 	}
