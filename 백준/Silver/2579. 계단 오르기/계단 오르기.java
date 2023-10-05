0cd3686a1b543d189500658b64adcb4bac2c6800
huni19541 <133645725+huni19541@users.noreply.github.com>
2023-10-05 10:58:38 +0900

    [Silver III] Title: 계단 오르기, Time: 128 ms, Memory: 14368 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,55 +1,34 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map.Entry;
-import java.util.Queue;
-import java.util.Set;
-import java.util.Stack;
-import java.util.StringTokenizer;
-
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	
+
 	public static void main(String[] args) throws IOException {
 		
-		//System.setIn(new FileInputStream("Test.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 		
 		int N = Integer.parseInt(br.readLine());
-		int[] scores = new int[N+1];
-		int[] dp = new int[N+1];
-		for(int i=1; i<=N; i++)
-			scores[i] = Integer.parseInt(br.readLine());
-		
-		dp[1] = scores[1];
-		if(N >= 2)
-			dp[2] = scores[2]+dp[1];
-		
-		for(int i=3; i<N; i++) {
-			if(i == N-1) {
-				dp[i] = dp[i-2]+scores[i];
-				continue;
-			}
-			dp[i] = Math.max(dp[i-3]+scores[i-1]+scores[i], dp[i-2]+scores[i]);
+		int[] arr = new int[301];
+		int[] dp = new int[301];
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
 		}
 		
-		if(N >= 2)
-			dp[N] = Math.max(dp[N-2]+scores[N], dp[N-1]+scores[N]);
+		dp[0] = arr[0];
+		dp[1] = arr[0]+arr[1];
+		dp[2] = Math.max(arr[0]+arr[2], arr[1]+arr[2]);
 		
-		sb.append(dp[N]);
+		for(int i=3; i<N; i++) {
+			dp[i] = Math.max(dp[i-2]+arr[i], dp[i-3]+arr[i-1]+arr[i]);
+		}
+		
+		sb.append(dp[N-1]);
+		
 		System.out.println(sb);
+		
 	}
-
+	
+	
 }
