d55d1091ce0bdcead24ea7e4facd36ded734e9e5
huni19541 <133645725+huni19541@users.noreply.github.com>
2023-10-12 17:27:57 +0900

    [Gold IV] Title: Puyo Puyo, Time: 124 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Gold/11559. Puyo Puyo/Puyo Puyo.java
+++ 백준/Gold/11559. Puyo Puyo/Puyo Puyo.java
@@ -5,7 +5,6 @@ public class Main {
 	
 	static int[] dr = {-1, 0, 1, 0}; // 상 우 하 좌
 	static int[] dc = {0, 1, 0, -1};
-	static char[][] map;
 	
 	public static void main(String[] args) throws IOException {
 		
@@ -13,7 +12,7 @@ public class Main {
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 		
-		map = new char[12][6];
+		char[][] map = new char[12][6];
 		for(int i=0; i<12; i++) {
 			String str = br.readLine();
 			for(int j=0; j<6; j++) {
@@ -21,27 +20,28 @@ public class Main {
 			}
 		}
 		
+		// 폭발이 일어나지 않을 때 까지 실행
 		int ans = 0;
 		while(true) {
 			boolean check = false;
 			boolean[][] visited = new boolean[12][6];
-			char[][] temp = new char[12][6];
-			for(int i=0; i<12; i++) {
-				for(int j=0; j<6; j++) {
-					temp[i][j] = map[i][j];
-				}
-			}
 			
+			// 4개 이상이 붙어 있는지 확인
 			for(int i=0; i<12; i++) {
 				for(int j=0; j<6; j++) {
 					if(map[i][j] != '.' && !visited[i][j]) {
 						char curChar = map[i][j];
+						
+						// 4개 이상이 붙어있는지 확인하기 위한 bfs
 						Queue<int[]> queue = new ArrayDeque<>();
 						queue.offer(new int[] {i, j});
+						
+						// 터져야 할 뿌요들의 좌표를 저장할 list
 						ArrayList<int[]> list = new ArrayList<>();
 						list.add(new int[] {i, j});
 						visited[i][j] = true;
 						
+						// bfs 탐색
 						while(!queue.isEmpty()) {
 							int[] cur = queue.poll();
 							
@@ -57,19 +57,22 @@ public class Main {
 							}	
 						}
 						
+						// 4개 이상이 붙어있는 경우 폭발
 						if(list.size() >= 4) {
 							if(!check) ans++;
 							check = true;
 							for(int[] arr : list) {
-								temp[arr[0]][arr[1]] = '.';
+								map[arr[0]][arr[1]] = '.';
 							}
 						}
 					}
 				}
 			}
 			
+			// 폭발이 일어나지 않을 경우 break
 			if(!check) break;
 			
+			// 폭발 후 map 내리기
 			int[] idx = new int[6];
 			for(int i=0; i<6; i++) {
 				idx[i] = 11;
@@ -77,16 +80,14 @@ public class Main {
 			
 			for(int j=0; j<6; j++) {
 				for(int i=11; i>=0; i--) {
-					if(temp[i][j] != '.' && i != idx[j]) {
-						temp[idx[j]--][j] = temp[i][j];
-						temp[i][j] = '.';
-					} else if(temp[i][j] != '.' && i == idx[j]) {
+					if(map[i][j] != '.' && i != idx[j]) {
+						map[idx[j]--][j] = map[i][j];
+						map[i][j] = '.';
+					} else if(map[i][j] != '.' && i == idx[j]) {
 						idx[j]--;
 					}
 				}
 			}
-			
-			map = temp;
 		}
 		sb.append(ans);
 		
