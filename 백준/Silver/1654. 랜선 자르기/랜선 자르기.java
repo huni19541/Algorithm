63a2687d29372e12f020b7dd5d5b110e2f4e35cd
huni19541 <133645725+huni19541@users.noreply.github.com>
2024-01-08 22:54:33 +0900

    [Silver II] Title: 랜선 자르기, Time: 180 ms, Memory: 17208 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,72 +1,46 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map.Entry;
-import java.util.Queue;
-import java.util.Set;
-import java.util.Stack;
-import java.util.StringTokenizer;
-
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		
-		//System.setIn(new FileInputStream("Test.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		
-		st = new StringTokenizer(br.readLine());
-		int K = Integer.parseInt(st.nextToken());
-		int N = Integer.parseInt(st.nextToken());
-		
-		Integer[] lens = new Integer[K];
-		long totalLen = 0L;
-		for(int i=0; i<K; i++) {
-			lens[i] = Integer.parseInt(br.readLine());
-			totalLen += lens[i];
-		}
-		Arrays.sort(lens, Collections.reverseOrder());
-		
-		//Long max = totalLen / N;
-		//if(max > lens[lens.length-1]) max = lens[lens.length-1];
-		
-		int idx = lens.length-1;
-		int res = Integer.MAX_VALUE;
-		int max = 0;
-		while(res > N) {
-			max = lens[idx--];
-			
-			res = 0;
-			for(int i=0; i<K; i++) {
-				res += lens[i] / max;
-			}
-		}
-		max = lens[idx+1];
-		
-		res = 0;
-		
-		while(res < N) {
-			res = 0;
-			for(int i=0; i<K; i++) {
-				res += lens[i] / max;
-			}
-			max--;
-		}
-		
-		
-		System.out.println(max+1);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
+        st = new StringTokenizer(br.readLine());
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[K];
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<K; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            if(arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        long start = 1;
+        long end = max;
+        long answer = 0;
+        while(start <= end) {
+            long mid = (start + end) / 2;
+
+            long temp = 0;
+            for(int i=0; i<K; i++) {
+                temp += (arr[i] / mid);
+            }
+
+            if(temp < N) {
+                end = mid - 1;
+            }
+            else {
+                start = mid + 1;
+                answer = mid;
+            }
+        }
+
+        sb.append(answer);
+        System.out.println(answer);
+    }
 }
